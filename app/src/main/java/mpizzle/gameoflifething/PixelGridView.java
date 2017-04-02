e3716a8de7ef5709ff903d4ddd79619ee63e477d
mpizzzle <michael.770211@gmail.com>
2017-04-02 15:13:16 +0100

    rough grid finished

--- app/src/main/java/mpizzle/gameoflifething/PixelGridView.java
+++ app/src/main/java/mpizzle/gameoflifething/PixelGridView.java
@@ -13,9 +13,8 @@ import android.view.MotionEvent;
 public class PixelGridView extends View {
     private int numColumns, numRows;
     private int cellWidth, cellHeight;
-    private int mWidth, mHeight;
     private Paint blackPaint = new Paint();
-    private Paint yellowPaint = new Paint();
+    private Paint greenPaint = new Paint();
     private Paint whitePaint = new Paint();
     private boolean[][] grid;
 
@@ -25,9 +24,10 @@ public class PixelGridView extends View {
 
     public PixelGridView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
         blackPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        yellowPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        yellowPaint.setColor(Color.YELLOW);
+        greenPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        greenPaint.setColor(Color.GREEN);
         whitePaint.setStyle(Paint.Style.FILL_AND_STROKE);
         whitePaint.setColor(Color.WHITE);
         this.cellWidth = 100;
@@ -36,52 +36,15 @@ public class PixelGridView extends View {
         this.numColumns = 100;
         this.setWillNotDraw(false);
         grid = new boolean[numColumns][numRows];
-    }/*
-
-    public void setNumColumns(int numColumns) {
-        this.numColumns = numColumns;
-        calculateDimensions();
     }
-
-    public int getNumColumns() {
-        return numColumns;
-    }
-
-    public void setNumRows(int numRows) {
-        this.numRows = numRows;
-        calculateDimensions();
-    }
-
-    public int getNumRows() {
-        return numRows;
-    }
-*/
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        //calculateDimensions();
-    }/*
-
-    private void calculateDimensions() {
-        if (numColumns < 1 || numRows < 1) {
-            return;
-        }
-
-        cellWidth = 100;//getWidth() / numColumns;
-        cellHeight = 100;//getHeight() / numRows;
-
-        grid = new boolean[numColumns][numRows];
-
-        invalidate();
-    }*/
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(Color.GRAY);
-
-        if (numColumns == 0 || numRows == 0) {
-            return;
-        }
 
         int width = getWidth();
         int height = getHeight();
@@ -89,41 +52,32 @@ public class PixelGridView extends View {
         for (int i = 0; i < numColumns; i++) {
             for (int j = 0; j < numRows; j++) {
                 if (grid[i][j]) {
-
-                    canvas.drawRect(i * cellWidth, j * cellHeight,
-                            (i + 1) * cellWidth, (j + 1) * cellHeight,
-                            yellowPaint);
+                    canvas.drawRect(i * cellWidth, j * cellHeight, (i + 1) * cellWidth, (j + 1) * cellHeight, greenPaint);
                 }
             }
         }
 
         for (int i = 1; i < numColumns; i++) {
-            canvas.drawLine(i * cellWidth, 1, i * cellWidth, height, whitePaint);
-        }
-
-        for (int i = 1; i < numRows; i++) {
-            canvas.drawLine(1, i * cellHeight, width, i * cellHeight, whitePaint);
+            canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, whitePaint);
+            canvas.drawLine(0, i * cellHeight, width, i * cellHeight, whitePaint);
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            int column = (int)(event.getX() / cellWidth);
-            int row = (int)(event.getY() / cellHeight);
+            int x = (int)(event.getX() / cellWidth);
+            int y = (int)(event.getY() / cellHeight);
 
-            grid[column][row] = !grid[column][row];
+            grid[x][y] = !grid[x][y];
             invalidate();
         }
 
         return true;
     }
 
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        mWidth = View.MeasureSpec.getSize(widthMeasureSpec);
-        mHeight = View.MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(mWidth, mHeight);
+        setMeasuredDimension(100 * numColumns, 100 * numRows);
     }
 }
