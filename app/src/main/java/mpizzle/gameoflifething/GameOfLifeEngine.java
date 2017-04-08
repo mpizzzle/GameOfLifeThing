bd157423b9ebe36a096f8872d1d221fc9369aedc
mpizzzle <michael.770211@gmail.com>
2017-04-08 22:42:09 +0100

    adding heatmap palette

--- app/src/main/java/mpizzle/gameoflifething/GameOfLifeEngine.java
+++ app/src/main/java/mpizzle/gameoflifething/GameOfLifeEngine.java
@@ -1,11 +1,17 @@
 package mpizzle.gameoflifething;
 
+import java.util.Arrays;
+
+import static mpizzle.gameoflifething.GameOfLifeView.ROWS;
+import static mpizzle.gameoflifething.GameOfLifeView.COLUMNS;
+import static mpizzle.gameoflifething.GameOfLifeView.PADDING;
+
 /**
  * Created by mpizzle on 04/04/17.
  */
 public class GameOfLifeEngine {
     public static boolean[][] step(boolean[][] currentGen, boolean[][] nextGen, int rows, int columns) {
-
+        currentGen = copyCells(currentGen);
         for (int i = 1; i <= columns; ++i) {
             for (int j = 1; j <= rows; ++j) {
                 int neighbours = 0;
@@ -25,5 +31,54 @@ public class GameOfLifeEngine {
         }
 
         return nextGen;
+    }
+    public static int[][] step(int[][] currentGen, int[][] nextGen, int rows, int columns) {
+        currentGen = copyCells(currentGen);
+
+        for (int i = 1; i <= columns; ++i) {
+            for (int j = 1; j <= rows; ++j) {
+                int neighbours = 0;
+                neighbours += currentGen[i][j + 1] > 0? 1 : 0;
+                neighbours += currentGen[i][j  - 1] > 0 ? 1 : 0;
+                neighbours += currentGen[i + 1][j] > 0 ? 1 : 0;
+                neighbours += currentGen[i  - 1][j] > 0 ? 1 : 0;
+                neighbours += currentGen[i + 1][j + 1] > 0 ? 1 : 0;
+                neighbours += currentGen[i + 1][j  - 1] > 0 ? 1 : 0;
+                neighbours += currentGen[i  - 1][j + 1] > 0 ? 1 : 0;
+                neighbours += currentGen[i  - 1][j  - 1] > 0 ? 1 : 0;
+
+                if ((currentGen[i][j] > 0 && (neighbours == 2 || neighbours == 3)) || (!(currentGen[i][j] > 0) && neighbours == 3)) {
+                    nextGen[i][j] = (currentGen[i][j] != 16) ? ((currentGen[i][j] + 1) % 16) + 1 : 16;
+                }
+            }
+        }
+
+        return nextGen;
+    }
+
+    private static boolean[][] copyCells(boolean[][] cellGrid)
+    {
+        cellGrid[0] = Arrays.copyOf(cellGrid[COLUMNS - 1], ROWS + PADDING);
+        cellGrid[COLUMNS + 1] = Arrays.copyOf(cellGrid[1], ROWS + PADDING);
+
+        for (int i = 1; i <= ROWS; ++i) {
+            cellGrid[i][0] = cellGrid[i][ROWS  - 1];
+            cellGrid[i][ROWS + 1] = cellGrid[i][1];
+        }
+
+        return cellGrid;
+    }
+
+    private static int[][] copyCells(int[][] cellGrid)
+    {
+        cellGrid[0] = Arrays.copyOf(cellGrid[COLUMNS - 1], ROWS + PADDING);
+        cellGrid[COLUMNS + 1] = Arrays.copyOf(cellGrid[1], ROWS + PADDING);
+
+        for (int i = 1; i <= ROWS; ++i) {
+            cellGrid[i][0] = cellGrid[i][ROWS  - 1];
+            cellGrid[i][ROWS + 1] = cellGrid[i][1];
+        }
+
+        return cellGrid;
     }
 }
--- app/src/main/java/mpizzle/gameoflifething/GameOfLifeView.java
+++ app/src/main/java/mpizzle/gameoflifething/GameOfLifeView.java
@@ -6,23 +6,26 @@ import android.graphics.Canvas;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import java.util.Arrays;
 import java.util.Random;
 
 /**
  * Created by mpizzle on 01/04/17.
  */
 public class GameOfLifeView extends View {
-    private Paint[][] palette;
+    public static final int ROWS = 1000;
+    public static final int COLUMNS = 1000;
+    public static final int PADDING = 2;
+
+    private Paint[][] randomPalette;
+    private Paint[] heatPalette;
     private Paint p;
     private boolean[][] currentGeneration;
     private boolean[][] nextGeneration;
-    private final int ROWS = 1000;
-    private final int COLUMNS = 1000;
+    private int[][] currentGenerationHeated;
+    private int[][] nextGenerationHeated;
     private final int CELL_HEIGHT = 15;
     private final int CELL_WIDTH = 15;
     private final int SIZE_OF_PALETTE = 16;
-    private final int PADDING = 2;
 
     public GameOfLifeView(Context context) {
         this(context, null);
@@ -35,6 +38,7 @@ public class GameOfLifeView extends View {
 
         Random r = new Random();
         r.setSeed(System.currentTimeMillis());
+
         currentGeneration = new boolean[COLUMNS + PADDING][ROWS + PADDING];
 
         for (int i = 1; i <= COLUMNS; ++i) {
@@ -43,12 +47,43 @@ public class GameOfLifeView extends View {
             }
         }
 
-        palette = new Paint[SIZE_OF_PALETTE][SIZE_OF_PALETTE];
+        currentGenerationHeated = new int[COLUMNS + PADDING][ROWS + PADDING];
+
+        for (int i = 1; i <= COLUMNS; ++i) {
+            for (int j = 1; j <= ROWS; ++j) {
+                currentGenerationHeated[i][j] = r.nextInt(2);
+            }
+        }
+
+        randomPalette = new Paint[SIZE_OF_PALETTE][SIZE_OF_PALETTE];
 
         for (int i = 0; i < SIZE_OF_PALETTE; ++i) {
             for (int j = 0; j < SIZE_OF_PALETTE; ++j) {
-                palette[i][j] = new Paint();
-                palette[i][j].setARGB(255, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+                randomPalette[i][j] = new Paint();
+                randomPalette[i][j].setARGB(255, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+            }
+        }
+
+        heatPalette = new Paint[SIZE_OF_PALETTE];
+        int increment = 0;
+        int decrement = 0;
+        boolean blep = true;
+
+        for (int i = 0; i < SIZE_OF_PALETTE; ++i) {
+            heatPalette[i] = new Paint();
+            heatPalette[i].setARGB(255, increment, 255 - decrement, 0);
+            if (blep) {
+                increment += 32;
+            }
+            else {
+                decrement += 32;
+            }
+            if (increment > 255) {
+                increment = 255;
+                blep = false;
+            }
+            if (decrement  <  0) {
+                decrement = 0;
             }
         }
 
@@ -61,32 +96,23 @@ public class GameOfLifeView extends View {
     }
 
     public void setCustomIntProperty(int value){
-        currentGeneration = copyCells(currentGeneration);
-        nextGeneration = new boolean[COLUMNS + PADDING][ROWS + PADDING];
-        currentGeneration = GameOfLifeEngine.step(currentGeneration, nextGeneration, COLUMNS, ROWS);
+        //nextGeneration = new boolean[COLUMNS + PADDING][ROWS + PADDING];
+        //currentGeneration = GameOfLifeEngine.step(currentGeneration, nextGeneration, COLUMNS, ROWS);
+        nextGenerationHeated = new int[COLUMNS + PADDING][ROWS + PADDING];
+        currentGenerationHeated = GameOfLifeEngine.step(currentGenerationHeated, nextGenerationHeated, COLUMNS, ROWS);
         invalidate();
-    }
-
-    private boolean[][] copyCells(boolean[][] cellGrid)
-    {
-        cellGrid[0] = Arrays.copyOf(cellGrid[COLUMNS - 1], ROWS + PADDING);
-        cellGrid[COLUMNS + 1] = Arrays.copyOf(cellGrid[1], ROWS + PADDING);
-
-        for (int i = 1; i <= ROWS; ++i) {
-            cellGrid[i][0] = cellGrid[i][ROWS  - 1];
-            cellGrid[i][ROWS + 1] = cellGrid[i][1];
-        }
-
-        return cellGrid;
     }
 
         @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawARGB(255, 100, 100, 100);// 0, 0, 0);
+        canvas.drawARGB(255, 0, 0, 0);
         for (int i = 1; i <= COLUMNS; i++) {
             for (int j = 1; j <= ROWS; j++) {
-                if (currentGeneration[i][j]) {
-                    canvas.drawRect((i - 1) * CELL_WIDTH, (j - 1) * CELL_HEIGHT, i * CELL_WIDTH, j * CELL_HEIGHT, p);// palette[i % SIZE_OF_PALETTE][j % SIZE_OF_PALETTE]);
+                if (currentGenerationHeated[i][j] > 0) {
+                    //if (currentGeneration[i][j]) {
+                    //canvas.drawRect((i - 1) * CELL_WIDTH, (j - 1) * CELL_HEIGHT, i * CELL_WIDTH, j * CELL_HEIGHT, p);
+                    //canvas.drawRect((i - 1) * CELL_WIDTH, (j - 1) * CELL_HEIGHT, i * CELL_WIDTH, j * CELL_HEIGHT, randomPalette[i % SIZE_OF_PALETTE][j % SIZE_OF_PALETTE]);
+                    canvas.drawRect((i - 1) * CELL_WIDTH, (j - 1) * CELL_HEIGHT, i * CELL_WIDTH, j * CELL_HEIGHT, heatPalette[currentGenerationHeated[i][j] - 1]);
                 }
             }
         }
